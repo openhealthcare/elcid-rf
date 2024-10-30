a98c653a9323e7f9937383c007e2c14e73831dca
David Miller <david@deadpansincerity.com>
2024-10-30 10:48:34 +0000

    Bump Version Number

--- elcid/settings.py
+++ elcid/settings.py
@@ -419,7 +419,7 @@ else:
     EMAIL_HOST = 'localhost'
 
 
-VERSION_NUMBER = '0.125'
+VERSION_NUMBER = '0.126'
 
 #TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 #TEST_RUNNER = 'django_test_coverage.runner.CoverageTestSuiteRunner'
